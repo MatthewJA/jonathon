e31094e97817933c5aa06812aa6b8e62b7afbe11
MatthewJA <matthew.alger@gmail.com>
2019-01-05 09:14:24 +1100

    added debug stuff

--- watchathon.py
+++ watchathon.py
@@ -8,11 +8,12 @@ bens_global_state = {}
 
 def webhook_push():
     data = request.data
-    push = {
-        'author': data['pusher']['name'],
-        'time': data['pushed_at'],
-    }
-    if 'pushes' in bens_global_state:
-        bens_global_state['pushes'].append(push)
-    else:
-        bens_global_state['pushes'] = [push]
+    return repr(data)
+    # push = {
+    #     'author': data['pusher']['name'],
+    #     'time': data['pushed_at'],
+    # }
+    # if 'pushes' in bens_global_state:
+    #     bens_global_state['pushes'].append(push)
+    # else:
+    #     bens_global_state['pushes'] = [push]
