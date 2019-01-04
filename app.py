edbcbbe1444ae73b015ca313503d0cb1343b673d
MatthewJA <matthew.alger@gmail.com>
2019-01-04 23:50:07 +1100

    added web-hook

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+bens_global_state = {}
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello world!'
+def recent_pushes():
+    out = '<ul>'
+    for push in bens_global_state.get('push', []):
+        out += ('<li>{} @ {}</li>'.format(push['author'], push['time']))
+    out += '</ul>'
+    return out
 
 
 if __name__ == '__main__':
---
+++ watchathon.py
@@ -0,0 +1,17 @@
+"""Watch GitHub for events."""
+
+import flask
+from flask import request
+
+from app import bens_global_state
+
+def webhook_push():
+    data = request.data
+    push = {
+        'author': data['pusher']['name'],
+        'time': data['pushed_at'],
+    }
+    if 'pushes' in bens_global_state:
+        bens_global_state['pushes'].append(push)
+    else:
+        bens_global_state['pushes'] = [push]
