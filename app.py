e20a207b3be3c093c7e19da804d8d69263f8ca1b
MatthewJA <matthew.alger@gmail.com>
2019-01-10 14:40:20 +1100

    jonathon -> student tutorial

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
-from flask import Flask, request
-
-import watchathon
-from db import query, clear_all
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def recent_pushes():
-    out = '<ul>'
-    pushes = query('pushes')
-    for push in pushes:
-        out += ('<li>{} @ {}</li>'.format(push['author'], push['time']))
-    out += '</ul>'
-    return out
-
-
-push_hook = app.route('/push_hook', methods=['POST'])(watchathon.webhook_push)
-
-app.route('/clear_all')
-def clear_db():
-    clear_all()
-    return 'Cleared'
+@app.route('/git')
+def use_git():
+    return 'yeah'
 
 
 if __name__ == '__main__':
