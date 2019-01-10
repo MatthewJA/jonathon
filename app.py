07ded70e1737d7b5e6ad155ecefa921c7d3197b6
MatthewJA <matthew.alger@gmail.com>
2019-01-10 14:41:55 +1100

    added slack endpoint

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/git')
+@app.route('/usegit', methods=['GET', 'POST'])
 def use_git():
     return 'yeah'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
