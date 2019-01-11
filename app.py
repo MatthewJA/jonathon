2e87f16b9d732d9231d09c84b65a19bd93a7cf14
MatthewJA <matthew.alger@gmail.com>
2019-01-11 20:29:28 +1100

    added simple cloning script

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 """Watches git repositories for breakages."""
 
+import subprocess
+import shutil
+from pathlib import Path
+
 REPO_SSH = 'git@github.com:ncss/projects-2019-2d.git'
 BRANCH = 'dev'
+CLONE_DIR = Path('.cloned')
 
-def try_the_repo(repo_ssh=REPO_SSH, branch=BRANCH):
+def setup_repo(repo_ssh=REPO_SSH, branch=BRANCH, clone_dir=CLONE_DIR):
+    shutil.rmtree(clone_dir)
     # Clone the repo.
-    pass
+    subprocess.run(['git', 'clone', repo_ssh, clone_dir])
+    # Pull.
+    with clone_dir:
+        subprocess.run(['git', 'checkout', branch])
+
+# def try_repo():
+    
+if __name__ == '__main__':
+    setup_repo()
